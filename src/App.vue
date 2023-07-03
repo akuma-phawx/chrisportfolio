90fedb02033b8fafb7911e4c50c320fbc408c2f8
Christoforos Vradis <christoforos.vradis@onecentral.nl>
2023-07-03 20:04:35 +0200

    chore(): Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h2>The App</h2>
+  <router-link to="/">Go to Home</router-link>
+  <router-link to="/about">Go to About</router-link>
+  <router-view></router-view>
+</template>
+
